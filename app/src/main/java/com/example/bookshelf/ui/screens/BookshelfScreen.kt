794f5300f9594fee2d6a5af7ce572539622d5c4a
PaoloCaldera <paolo.caldera@outlook.com>
2024-03-29 00:41:53 +0100

    feature: implement the app layout in case of successful data retrieval
    Define the function BookshelfSuccessScreen to implement the application layout in case data are loaded correctly, to display on the screen as expected.
    Import inside the build.gradle.kts file the necessary dependencies (retrofit, serialization and coil) in order to convert the image from url to drawable.
    Insert the necessary dimension resources in the dimens.xml file.
    Define the data class BookshelfItem that will be used as the one containing the elements constituting the UI layer.
    
    Integrates #1

--- app/src/main/java/com/example/bookshelf/ui/screens/BookshelfScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/BookshelfScreen.kt
@@ -4,25 +4,36 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.bookshelf.R
+import com.example.bookshelf.model.BookshelfItem
 
 @Composable
 fun BookshelfLoadingScreen(modifier: Modifier = Modifier) {
     Image(
-        painter = painterResource(R.drawable.loading_img), 
+        painter = painterResource(R.drawable.loading_img),
         contentDescription = stringResource(R.string.loading_content_description),
         modifier = modifier.size(dimensionResource(R.dimen.loading_image_size))
     )
@@ -58,8 +69,38 @@ fun BookshelfErrorScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun BookshelfSuccessScreen(
+    bookshelfItems: List<BookshelfItem>,
     modifier: Modifier = Modifier
 ) {
+    LazyVerticalGrid(columns = GridCells.Adaptive(dimensionResource(R.dimen.list_column_width))) {
+        items(bookshelfItems) {
+            BookshelfListItem(
+                bookshelfItem = it,
+                modifier = modifier
+            )
+        }
+    }
+}
 
+@Composable
+fun BookshelfListItem(
+    bookshelfItem: BookshelfItem,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = dimensionResource(R.dimen.card_elevation))
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(bookshelfItem.image)
+                .crossfade(true)
+                .build(),
+            contentDescription = bookshelfItem.title,
+            placeholder = painterResource(R.drawable.loading_img),
+            error = painterResource(R.drawable.ic_broken_image),
+            contentScale = ContentScale.Crop,
+            modifier = modifier.fillMaxSize()
+        )
+    }
 }
 
